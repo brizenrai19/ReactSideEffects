2cb10d8dc5a1f9ccca3e4e85d6b7023324e1d0c8
unknown <brizen.rai19@gmail.com>
2021-04-08 16:26:28 -0400

    Initail Setup

---
+++ src/components/Details.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import { BASE_URL, API_KEY } from '../constants'
+import axios from 'axios'
+
+export default function Details(props) {
+  const { friendId, close } = props
+  const [details, setDetails] = useState(null)
+
+  // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
+
+  // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
+  // and puts a 'click' event handler on document.
+  // See what happens if we don't clean up.
+
+  // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
+
+  // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
+  // Whenever props.friendId updates we should trigger a fetch for details of the friend.
+  // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
+  // On success, shove the details of the friend in `details` slice of state
+
+  return (
+    <div className='container'>
+      <h2>Details (of friend with id {friendId}):</h2>
+      {
+        details &&
+        <>
+          <p>{details.name} is {details.age}</p>
+          <p>email is {details.email}</p>
+          {name} likes:
+          <ul>
+            {details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)}
+          </ul>
+        </>
+      }
+      <button onClick={close}>Close</button>
+    </div>
+  )
+}
